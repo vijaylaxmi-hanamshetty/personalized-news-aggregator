12fdc61bc1488b649a049660d6768bb6dd4bcea5
Vijaylaxmi Hanamshetty <vijaylaxmihanamshetty38@gmail.com>
2025-05-02 19:57:28 +0530

    Fix small mistake in setting update

--- Authentication/seralizers.py
+++ Authentication/seralizers.py
@@ -1,38 +1,34 @@
 from rest_framework import serializers
-from django.contrib.auth import get_user_model
-from rest_framework.exceptions import ValidationError
+from .models import User
+from django.contrib.auth import authenticate
 
-User = get_user_model()
 
 class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["id", "phone", "name", "role", "password"]
-        extra_kwargs = {"password": {"write_only": True}}
+        fields = ['phone', 'name', 'password', 'role']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
 
     def create(self, validated_data):
-        user = User(
-            phone=validated_data["phone"],
-            name=validated_data["name"],
-            role=validated_data["role"],
-        )
-        user.set_password(validated_data["password"])
-        user.save()
-        return user
+        return User.objects.create_user(**validated_data)
+
 
 class LoginSerializer(serializers.Serializer):
     phone = serializers.CharField()
-    password = serializers.CharField()
+    password = serializers.CharField(write_only=True)
 
-    def validate(self, attrs):
-        phone = attrs.get("phone")
-        password = attrs.get("password")
+    def validate(self, data):
+        phone = data.get('phone')
+        password = data.get('password')
 
-        user = User.objects.filter(phone=phone).first()
-        if user is None:
-            raise ValidationError("User with this phone number does not exist.")
-
-        if not user.check_password(password):
-            raise ValidationError("Invalid password.")
-
-        return attrs
+        if phone and password:
+            user = authenticate(request=self.context.get('request'), phone=phone, password=password)
+            if not user:
+                raise serializers.ValidationError("Invalid phone or password.")
+        else:
+            raise serializers.ValidationError("Must include 'phone' and 'password'.")
+        
+        data['user'] = user
+        return data
