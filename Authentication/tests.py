f3420e00cd67686a661ca8b99ccc525d1a0f2413
Vijaylaxmi Hanamshetty <vijaylaxmihanamshetty38@gmail.com>
2025-05-06 16:15:45 +0530

    Update autentication   test cases

--- Authentication/tests.py
+++ Authentication/tests.py
@@ -1,4 +1,4 @@
-from unittest.mock import patch, MagicMock
+from unittest.mock import patch
 from django.test import TestCase
 from rest_framework.test import APIClient
 from django.contrib.auth import get_user_model
@@ -29,12 +29,9 @@ class AuthenticationTestCase(TestCase):
             "password": "wrongpassword",
         }
 
-    @patch("twilio.rest.Client.messages") 
-    def test_register_user_success(self, mock_twilio):
-      
-        mock_message = MagicMock()
-        mock_message.sid = "mock_sid"
-        mock_twilio.create.return_value = mock_message
+    def test_register_user_success(
+        self,
+    ):
 
         # Perform the registration request
         response = self.client.post(self.register_url, self.valid_user_data)
@@ -47,60 +44,48 @@ class AuthenticationTestCase(TestCase):
         self.assertEqual(response.data["user"]["name"], self.valid_user_data["name"])
         self.assertEqual(response.data["user"]["role"], self.valid_user_data["role"])
 
-    def test_register_user_invalid_phone(self):
-        response = self.client.post(self.register_url, self.invalid_phone_data)
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertIn("detail", response.data) 
-        self.assertIn("Phone number parsing error", response.data["detail"])
-
-    @patch("twilio.rest.Client.messages")  
-    def test_login_user_success(self, mock_twilio):
-  
-     mock_message = MagicMock()
-     mock_message.sid = "mock_sid"
-     mock_twilio.create.return_value = mock_message
-
-    # Create a user for login
-     User.objects.create_user(
-         phone=self.valid_user_data["phone"],
-         password=self.valid_user_data["password"],
-        name=self.valid_user_data["name"],
-        role=self.valid_user_data["role"]
-    )
-
-   
-     login_data = {
-        "phone": self.valid_user_data["phone"],
-        "password": self.valid_user_data["password"]
-    }
-     response = self.client.post(self.login_url, login_data)
-
-    # Assertions
-     self.assertEqual(response.status_code, status.HTTP_200_OK)
-     self.assertIn("message", response.data)
-     self.assertEqual(response.data["message"], "Login successful.")
-     self.assertIn("user", response.data)
-     self.assertEqual(response.data["user"]["phone"], self.valid_user_data["phone"])
-     self.assertEqual(response.data["user"]["role"], self.valid_user_data["role"])
-     self.assertIn("access", response.data)  
-     self.assertIn("refresh", response.data)  
-     self.assertTrue(len(response.data["access"]) > 0)  
-     self.assertTrue(len(response.data["refresh"]) > 0)  
     
-    def test_login_user_invalid_credentials(self):
-     
+    def test_login_user_success(self):
+
+        # Create a user for login
         User.objects.create_user(
             phone=self.valid_user_data["phone"],
             password=self.valid_user_data["password"],
             name=self.valid_user_data["name"],
-            role=self.valid_user_data["role"]
+            role=self.valid_user_data["role"],
+        )
+
+        login_data = {
+            "phone": self.valid_user_data["phone"],
+            "password": self.valid_user_data["password"],
+        }
+        response = self.client.post(self.login_url, login_data)
+
+        # Assertions
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn("message", response.data)
+        self.assertEqual(response.data["message"], "Login successful.")
+        self.assertIn("user", response.data)
+        self.assertEqual(response.data["user"]["phone"], self.valid_user_data["phone"])
+        self.assertEqual(response.data["user"]["role"], self.valid_user_data["role"])
+        self.assertIn("access", response.data)
+        self.assertIn("refresh", response.data)
+        self.assertTrue(len(response.data["access"]) > 0)
+        self.assertTrue(len(response.data["refresh"]) > 0)
+
+    def test_login_user_invalid_credentials(self):
+
+        User.objects.create_user(
+            phone=self.valid_user_data["phone"],
+            password=self.valid_user_data["password"],
+            name=self.valid_user_data["name"],
+            role=self.valid_user_data["role"],
         )
         response = self.client.post(self.login_url, self.invalid_login_data)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertIn("non_field_errors", response.data)
         self.assertEqual(
-            response.data["non_field_errors"][0],
-            "Invalid phone or password."
+            response.data["non_field_errors"][0], "Invalid phone or password."
         )
 
     def test_login_user_invalid_phone_format(self):
@@ -108,6 +93,5 @@ class AuthenticationTestCase(TestCase):
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertIn("non_field_errors", response.data)
         self.assertIn(
-            "Invalid phone or password.",
-            response.data["non_field_errors"][0]
+            "Invalid phone or password.", response.data["non_field_errors"][0]
         )
