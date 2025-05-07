4efdc6540628e3415d15821cfc0713caa1e88048
Vijaylaxmi Hanamshetty <vijaylaxmihanamshetty38@gmail.com>
2025-05-07 12:00:17 +0530

    Basic setup  News

---
+++ Newsource/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class NewsourceConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "Newsource"
--- config/settings.py
+++ config/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     "drf_spectacular",
     "Authentication",
     "Profile",
+    "NewsSource"
 ]
 
 MIDDLEWARE = [
