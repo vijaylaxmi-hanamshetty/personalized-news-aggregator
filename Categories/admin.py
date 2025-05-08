2b4fbfed47790d61218c62f5918a050ce5a301d8
Vijaylaxmi Hanamshetty <vijaylaxmihanamshetty38@gmail.com>
2025-05-08 09:15:32 +0530

    Add models for Categories

--- Categories/admin.py
+++ Categories/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from Categories.models import Categories
 # Register your models here.
+admin.register(Categories)
--- Categories/models.py
+++ Categories/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Categories(models.Model):
+    id = models.AutoField(primary_key=True)  
+    name = models.CharField(max_length=255)  
+    slug = models.SlugField(unique=True)  
+    created_at = models.DateTimeField(auto_now_add=True)  
+
+    def __str__(self):
+        return self.name
