e127e2ad2af4a77d6251a3f26ac60c0e062654f2
Vijaylaxmi Hanamshetty <vijaylaxmihanamshetty38@gmail.com>
2025-05-08 09:28:51 +0530

    Add models for comments

--- Comments/models.py
+++ Comments/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+from Articles.models import Articles
 
-# Create your models here.
+class Comments(models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+    article = models.ForeignKey(Articles, on_delete=models.CASCADE, related_name="comments")
+    comment_text = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment by {self.user.username} on {self.article.title}"
