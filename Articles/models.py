ef68d061902f629279d8f9afadd16f5fecff1575
Vijaylaxmi Hanamshetty <vijaylaxmihanamshetty38@gmail.com>
2025-05-08 09:21:51 +0530

    Add models for Articles

--- Articles/models.py
+++ Articles/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from News.models import News
+from Categories.models import Categories
 
-# Create your models here.
+class Articles(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    summary = models.TextField(blank=True, null=True)
+    url = models.URLField()
+    published_at = models.DateTimeField()
+    source = models.ForeignKey(News, on_delete=models.CASCADE, related_name="articles")
+    category = models.ForeignKey(Categories, on_delete=models.CASCADE, related_name="articles")
+    language = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
