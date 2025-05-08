5c557aca542af92b2aa18e6faf59e5d64f8229d9
Vijaylaxmi Hanamshetty <vijaylaxmihanamshetty38@gmail.com>
2025-05-08 09:34:59 +0530

    Add models for savearticles

--- Savearticles/admin.py
+++ Savearticles/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from Savearticles.models import SavedArticles
 # Register your models here.
+admin.register(SavedArticles)
--- Savearticles/models.py
+++ Savearticles/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+from Articles.models import Articles
 
-# Create your models here.
+class SavedArticles(models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="saved_articles")
+    article = models.ForeignKey(Articles, on_delete=models.CASCADE, related_name="saved_by_users")
+    saved_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} saved {self.article.title}"
