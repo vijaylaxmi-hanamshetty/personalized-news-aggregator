b373987b41582c1d3ae3b3e3bdda2aaece1d9206
Vijaylaxmi hanamshetty <vijaylaxmihanamshetty38@gmail.com>
2025-05-16 20:03:55 +0530

    Add initial migrations

--- Profile/models.py
+++ Profile/models.py
@@ -4,7 +4,7 @@ from  Authentication.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
     bio = models.TextField(blank=True, null=True)  
-    profile_picture =  models.ImageField(upload_to='uploads/') 
+    profile_picture =  models.ImageField(upload_to='uploads/',default='profiles/default.jpg') 
     location = models.CharField(max_length=100, blank=True, null=True)
     birth_date = models.DateField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
