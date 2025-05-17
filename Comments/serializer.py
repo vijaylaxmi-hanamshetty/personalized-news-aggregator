b9dc900e0c689a06f5d197e3b00be8da62eb5ed5
Vijaylaxmi hanamshetty <vijaylaxmihanamshetty38@gmail.com>
2025-05-17 13:42:39 +0530

    Connect comments to article_id

--- Comments/serializer.py
+++ Comments/serializer.py
@@ -1,11 +1,11 @@
 from rest_framework import serializers
 from Comments.models import Comments
-
+from Articles.models import Articles
 
 class CommentsSerializer(serializers.ModelSerializer):
     user = serializers.StringRelatedField(read_only=True)  
-    article = serializers.StringRelatedField(read_only=True)
+    article = serializers.PrimaryKeyRelatedField(queryset=Articles.objects.all())
     class Meta:
         model = Comments
         fields = '__all__'
-    read_only_fields = ['user', 'timestamp']   
+        read_only_fields = ['user', 'timestamp']   
